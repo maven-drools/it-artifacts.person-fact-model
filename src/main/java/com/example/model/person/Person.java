2088dba48092edc1b4359072c25de52a215954be
Ansgar Konermann <ansgar.konermann@googlemail.com>
2011-03-06 03:28:33 +0100

    [MDP-3] Package move: com.example.model -> com.example.model.person; Adding new model com.example.model.location (city, country, continent, distance).

--- src/main/java/com/example/model/person/Person.java
+++ src/main/java/com/example/model/person/Person.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.model;
+package com.example.model.person;
 
 public class Person {
 
